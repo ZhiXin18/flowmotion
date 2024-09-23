2f94fe4954edef6d2324342c73086abb8b4e9f89
ZhiXin18 <t.zhixin18@gmail.com>
2024-09-23 10:04:51 +0800

    feat(test): implement integration to test authorized login flow

---
+++ integration_test/e2e_auth_test.dart
@@ -0,0 +1,31 @@
+import 'package:flowmotion/screens/loginScreen.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:flowmotion/main.dart' as app;
+
+import '../test_auth_info.dart';
+import 'robots/login_robot.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  late LoginRobot loginRobot;
+
+  group('E2E - ', () {
+    testWidgets("Authorized Flow", (tester) async {
+      await tester.pumpWidget(app.MyApp());
+      loginRobot = LoginRobot(tester: tester);
+
+      await tester.pumpAndSettle(Duration(seconds: 4));
+      loginRobot.verify();
+      await loginRobot.enterEmail(TestAuthInfo.authTestEmailEnv);
+      await Future.delayed(const Duration(seconds: 2));
+      await loginRobot.enterPassword(TestAuthInfo.authTestPasswordEnv);
+      await Future.delayed(const Duration(seconds: 2));
+      await loginRobot.tapLoginButton();
+      await Future.delayed(const Duration(seconds: 5));
+      await tester.pumpAndSettle();
+      loginRobot.verifySuccess();
+    });
+  });
+}
