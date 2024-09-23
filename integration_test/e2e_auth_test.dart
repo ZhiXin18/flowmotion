d152dc2790a94974038629d940abd017413a4889
ZhiXin18 <t.zhixin18@gmail.com>
2024-09-23 10:11:46 +0800

    feat(test): implement integration test to test authorized login flow

--- integration_test/e2e_auth_test.dart
+++ integration_test/e2e_auth_test.dart
@@ -1,4 +1,3 @@
-import 'package:flowmotion/screens/loginScreen.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flowmotion/main.dart' as app;
