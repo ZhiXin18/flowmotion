60fc5055aee4b727edb9cd7593b7936b7cb50061
ZhiXin18 <t.zhixin18@gmail.com>
2024-09-24 17:10:41 +0800

    feat(ui): implement widget keys for register screen

--- lib/core/widget_keys.dart
+++ lib/core/widget_keys.dart
@@ -11,6 +11,17 @@ class WidgetKeys {
   static const Key loginButton = Key('loginButton');
   static const Key loginErrorSnackbar = Key('loginErrorSnackbar');
   static const Key loginErrorDialog = Key('loginErrorDialog');
+  static const Key loginErrorOK = Key('loginErrorOK');
+
+  //register screen widgets
+  static const Key goRegisterButton = Key('goRegisterButton');
+  static const Key registerScreen = Key('registerScreen');
+  static const Key registerNameController = Key('registerNameController');
+  static const Key registerEmailController = Key('registerEmailController');
+  static const Key registerPasswordController = Key('registerPasswordController');
+  static const Key registerButton = Key('registerButton');
+  static const Key registerErrorDialog = Key('registerErrorDialog');
+  static const Key registerErrorOK = Key('registerErrorOK');
 
   //home screen widgets
   static const Key homeScreen = Key('homeScreen');
