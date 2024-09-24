2c0106800ab809dfc425890a41a2ef7004859b52
ZhiXin18 <t.zhixin18@gmail.com>
2024-09-24 11:08:27 +0800

    feat(test): implement integration test to test unauthorized login flow

--- integration_test/e2e_unauth_test.dart
+++ integration_test/e2e_unauth_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flowmotion/main.dart' as app;
-
 import '../test_auth_info.dart';
 import 'robots/login_robot.dart';
 
