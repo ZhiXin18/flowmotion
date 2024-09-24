333cbaa56148110e86768dde4f237efd2401714b
ZhiXin18 <t.zhixin18@gmail.com>
2024-09-24 11:00:00 +0800

    feat(test): feat(test): implement integration test to test unauthorized login flow

---
+++ integration_test/e2e_unauth_test.dart
@@ -0,0 +1,30 @@
+import 'package:flowmotion/screens/loginScreen.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:flowmotion/main.dart' as app;
+
+import '../test_auth_info.dart';
+import 'robots/login_robot.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  late LoginRobot loginRobot;
+
+  group('E2E - ', () {
+    testWidgets("Unauthorized Login Flow", (tester) async {
+      await tester.pumpWidget(app.MyApp());
+      loginRobot = LoginRobot(tester: tester);
+
+      await tester.pumpAndSettle(Duration(seconds: 4));
+      loginRobot.verify();
+      await loginRobot.enterEmail(TestAuthInfo.unauthTestEmailEnv);
+      await Future.delayed(const Duration(seconds: 2));
+      await loginRobot.enterPassword(TestAuthInfo.unauthTestEmailEnv);
+      await Future.delayed(const Duration(seconds: 2));
+      await loginRobot.tapLoginButton();
+      await Future.delayed(const Duration(seconds: 2));
+      loginRobot.verifyError();
+    });
+  });
+}
