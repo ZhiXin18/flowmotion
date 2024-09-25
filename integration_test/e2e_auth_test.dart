b91c7eb1e521293f05048d389de500224c145fde
ZhiXin18 <t.zhixin18@gmail.com>
2024-09-25 20:59:20 +0800

    feat(test): implement integration test and robot for navigation to full map screen

--- integration_test/e2e_auth_test.dart
+++ integration_test/e2e_auth_test.dart
@@ -3,12 +3,14 @@ import 'package:integration_test/integration_test.dart';
 import 'package:flowmotion/main.dart' as app;
 
 import '../test_auth_info.dart';
+import 'robots/home_robot.dart';
 import 'robots/login_robot.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   late LoginRobot loginRobot;
+  late HomeRobot homeRobot;
 
   group('E2E - ', () {
     testWidgets("Authorized Login Flow", (tester) async {
@@ -22,9 +24,30 @@ void main() {
       await loginRobot.enterPassword(TestAuthInfo.authTestPasswordEnv);
       await Future.delayed(const Duration(seconds: 2));
       await loginRobot.tapLoginButton();
-      await Future.delayed(const Duration(seconds: 2));
-      await tester.pumpAndSettle();
+      await tester.pumpAndSettle(const Duration(seconds: 2));
       loginRobot.verifySuccess();
+    });
+
+    testWidgets("Authorized Login To Map Flow", (tester) async {
+      await tester.pumpWidget(const app.MyApp());
+      loginRobot = LoginRobot(tester: tester);
+      homeRobot = HomeRobot(tester: tester);
+
+      await tester.pumpAndSettle(const Duration(seconds: 4));
+      loginRobot.verify();
+      await loginRobot.enterEmail(TestAuthInfo.authTestEmailEnv);
+      await Future.delayed(const Duration(seconds: 2));
+      await loginRobot.enterPassword(TestAuthInfo.authTestPasswordEnv);
+      await Future.delayed(const Duration(seconds: 2));
+      await loginRobot.tapLoginButton();
+      await Future.delayed(const Duration(seconds: 2));
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+      loginRobot.verifySuccess();
+
+      //click view full map
+      await homeRobot.tapFullMapButton();
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+      homeRobot.verifyFullMap();
     });
   });
 }
