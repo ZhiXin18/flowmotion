4d102bbbca395aac2c8284cf2cff7cd4b6657927
devlinwaluja <81210186+devlinwaluja@users.noreply.github.com>
2024-09-27 13:34:45 +0800

    Update and rename APIClient.py to api.py

--- pipeline/api.py
+++ pipeline/api.py
@@ -25,26 +25,14 @@ class APIClient():
         print(f"The API was called at: {self.timestamp}")
 
     def extract_image(self, camera_id):
-        # Debugging step to ensure camera_id is correctly parsed
-        #print(f"Searching for camera_id: {camera_id}")
-
         # Loop through the items and cameras to find the correct camera_id
         for item in self.metadata["items"]:
             for camera in item["cameras"]:
-                # Debugging step to print the camera_id being checked
-                #print(f"Checking camera_id: {camera['camera_id']}")
-                
                 if camera["camera_id"] == str(camera_id):
-                    #print(f"Found camera_id: {camera_id}")
                     return camera["image"]  # Return the image URL if the camera ID matches
-
         # If camera ID is not found
         return f"Camera ID {camera_id} not found."
 
-#Debugging
-#client = APIClient(API_URL)
-#image_url = client.extract_image(1111)
-#print("Camera Image URL:", image_url)
 
  
 
