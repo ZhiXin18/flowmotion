8d89bfc4801920c63e84f41cbf2993425c4d74c3
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-09-27 12:52:35 +0800

    Delete scrape.py

--- scrape.py
+++
@@ -1,9 +0,0 @@
-import json 
-import requests
-
-url = "https://api.data.gov.sg/v1/transport/traffic-images"
-response = requests.get(url)
-
-print(type(response.json()))
-with open('metadata.json', 'w') as json_file:
-    json.dump(response.json(), json_file)
