5883886037933361ac8040333459b6982eb91722
ZhiXin18 <t.zhixin18@gmail.com>
2024-09-29 22:11:32 +0800

    feat(congestion): added the feature to read from database and plot congestion rating

--- lib/screens/fullMapScreen.dart
+++ lib/screens/fullMapScreen.dart
@@ -1,8 +1,11 @@
 import 'package:flowmotion/core/widget_keys.dart';
+import 'package:flowmotion/utilities/firebase_calls.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import '../models/congestion_rating.dart';
 import '../widgets/navigationBar.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FullMapScreen extends StatefulWidget {
   const FullMapScreen({super.key});
@@ -12,6 +15,19 @@ class FullMapScreen extends StatefulWidget {
 }
 
 class _FullMapScreenState extends State<FullMapScreen> {
+  List<CongestionRating> congestionRatings = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCongestionRatings();
+  }
+
+  Future<void> _fetchCongestionRatings() async {
+    congestionRatings = await FirebaseCalls().getCongestionRatings();
+    print("Fetched congestion ratings: $congestionRatings"); // Debug statement
+    setState(() {}); // Update UI after fetching data
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,21 +47,36 @@ class _FullMapScreenState extends State<FullMapScreen> {
       ),
       children: [
         openStreetMapTileLayer,
-        MarkerLayer(markers: [
-          Marker(
-            point: LatLng(1.2878, 103.8666),
-            width: 60,
-            height: 60,
-            alignment: Alignment.centerLeft,
-            child: Icon(
-              Icons.circle,
-              size: 15,
-              color: Colors.red,
-            )
-          )
-        ])
+        MarkerLayer(markers: _buildMarkers()),
       ],
     );
+  }
+
+  List<Marker> _buildMarkers() {
+    print("Building markers for congestion ratings: $congestionRatings"); // Debug statement
+    return congestionRatings.map((congestionRating) {
+      return Marker(
+        point: LatLng(congestionRating.latitude, congestionRating.longitude),
+        width: 60,
+        height: 60,
+        alignment: Alignment.centerLeft,
+        child: Icon(
+          Icons.circle,
+          size: 15, // Increased size for visibility
+          color: _getMarkerColor(congestionRating.value),
+        ),
+      );
+    }).toList(); // Convert iterable to list
+  }
+
+  Color _getMarkerColor(double value) {
+    if (value < 0.3) {
+      return Colors.green;
+    } else if (value < 0.7) {
+      return Colors.yellow;
+    } else {
+      return Colors.red;
+    }
   }
 }
 
