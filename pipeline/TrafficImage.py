a2a6ccdf224fc1c6460cfff8a8f98237f72b7ab0
devlinwaluja <81210186+devlinwaluja@users.noreply.github.com>
2024-09-30 21:30:30 +0800

    Update TrafficImage.py

--- pipeline/TrafficImage.py
+++ pipeline/TrafficImage.py
@@ -1,10 +1,11 @@
 import APIClient
 
 class TrafficImage:
-    def __init__(self, image_url,processed=False, congestion_rating=None):
+    def __init__(self, image_url,processed=False, congestion_rating=None, camera_id="Not Set!"):
         self.image_url = image_url
         self.processed = processed
         self.conestion_rating = congestion_rating
+        self.camera_id = camera_id
 
     def is_processed(self):
         self.processed = True
