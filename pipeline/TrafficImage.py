b14cf43aeffcab4a23a471ad61e4f7b55accba80
devlinwaluja <81210186+devlinwaluja@users.noreply.github.com>
2024-10-01 20:30:02 +0800

    Update TrafficImage.py

--- pipeline/TrafficImage.py
+++ pipeline/TrafficImage.py
@@ -1,13 +1,14 @@
 import APIClient
 
 class TrafficImage:
-    def __init__(self, image=None,processed=False, congestion_rating=None, camera_id="Not Set!", longitude="Longitude not set", latitude="Latitude not set"):
+    def __init__(self, image=None,processed=False, congestion_rating=None, camera_id="Not Set!", longitude="Longitude not set", latitude="Latitude not set", timestamp=None):
         self.image = image #numpy array
         self.processed = processed
         self.conestion_rating = congestion_rating
         self.camera_id = camera_id
         self.longitude = longitude
         self.latitude = latitude
+        self.timestamp = timestamp
 
     def processed(self, congestion_rating):
         self.processed = True
