05f74c1ff8ffba727b488d20235edc91abbd356e
devlinwaluja <81210186+devlinwaluja@users.noreply.github.com>
2024-10-02 17:24:25 +0800

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,9 +4,11 @@
 #
 
 if __name__ == "__main__":
-    # call api and populate TrafficImage objects.
-    # create an array of TrafficImage objects
-    # Pass arrray through the ML model
-        #-> ensure model calls .processed()
+    # Call API and populate TrafficImage objects
+    # Create an array of TrafficImage objects
+    # Pass array through the ML model
+    # Ensure model calls .processed()
+    pass  # Placeholder to indicate this block is empty for now
+
     
     
