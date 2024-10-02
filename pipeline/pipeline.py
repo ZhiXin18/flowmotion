4d3a9bff8f70ce571677f07eca371d2a3595f648
Zhu Zhanyan <program.nom@gmail.com>
2024-10-02 10:55:07 +0800

    revert: "Merge pull request #10 from ZhiXin18/Devlin", "Update.api.py", "Update TrafficImage.py"
    This reverts commits 99ec42a1bed259f6d72f9a4183943ab7a62d1330 & 60b28d887e5d54c1d64a9569b79de6a691cc53e6.
    This also reverts merge commit 22e7ead9740c37cfbb1ac205fad0499136bdd3eb, reversing
    changes made to a6cfa407cb7d45bfa33630c0f2734e8da3fd2265.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,9 +4,5 @@
 #
 
 if __name__ == "__main__":
-    # call api and populate TrafficImage objects.
-    # create an array of TrafficImage objects
-    # Pass arrray through the ML model
-        #-> ensure model calls .processed()
-    
-    
+    # TODO: pipeline code starts here
+    pass
