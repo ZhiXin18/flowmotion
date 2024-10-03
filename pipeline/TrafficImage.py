95401a1c58cd63e0e8902790f7d2dd430382ffde
devlinwaluja <81210186+devlinwaluja@users.noreply.github.com>
2024-10-03 18:19:26 +0800

    Update TrafficImage.py

--- pipeline/TrafficImage.py
+++ pipeline/TrafficImage.py
@@ -1,16 +1,14 @@
-
-
 class TrafficImage:
     def __init__(self, image=None,processed=False, congestion_rating=None, camera_id="Not Set!", longitude="Longitude not set", latitude="Latitude not set"):
-        self.image = image #numpy array
+        self.image = image 
         self.processed = processed
-        self.conestion_rating = congestion_rating
+        self.congestion_rating = congestion_rating
         self.camera_id = camera_id
         self.longitude = longitude
         self.latitude = latitude
 
-    def processed(self, congestion_rating):
+
+
+    def set_processed(self, congestion_rating):
         self.processed = True
         self.conestion_rating = congestion_rating
-
-
