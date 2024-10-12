43aebb0103776cc3bc71d3efbc3cebc3718e3c53
F-R-YEO <yeofengren@gmail.com>
2024-10-12 17:26:24 +0800

    code1 car imageclassification

--- pipeline/yolov8custommodel/code2/train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
-
-model.train(data='config.yaml', epochs=50, imgsz=640, project='runs/segment', name = 'train2')
