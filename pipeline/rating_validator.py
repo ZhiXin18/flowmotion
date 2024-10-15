b63b57bed319685c115f0885657261a370965d33
devlinwaluja <devlin.waluja@gmail.com>
2024-10-15 12:12:42 +0800

    fixed validator logic

--- pipeline/rating_validator.py
+++ pipeline/rating_validator.py
@@ -1,16 +1,18 @@
-from TrafficImage import TrafficImage
-
-
 class RatingValidator:
     def __init__(self, TrafficImageObject: TrafficImage = None):
         self.traffic_image_object = TrafficImageObject
-        self.congestion_rating = TrafficImageObject.congestion_rating
-        self.processed = TrafficImageObject.processed 
+        # Only assign congestion_rating and processed if TrafficImageObject is not None
+        if TrafficImageObject is not None:
+            self.congestion_rating = TrafficImageObject.congestion_rating
+            self.processed = TrafficImageObject.processed
+        else:
+            self.congestion_rating = None
+            self.processed = None
 
     def validate(self) -> None:
-        if self.traffic_image_object == None:
+        if self.traffic_image_object is None:
             raise Exception("No Traffic Image Parsed Through Validatior!!!")
         elif self.congestion_rating < 0 or self.congestion_rating > 1:
             raise Exception("Congestion Rating Is Invalid (< 0 or > 1)!!!")
-        elif self.processed == False:
+        elif self.processed is False:
             raise Exception("This camera has not been processed through the ML model!!!")
