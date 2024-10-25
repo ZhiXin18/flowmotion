c982c24a767f633316bbaed27fa5c72ec446d786
Zhu Zhanyan <program.nom@gmail.com>
2024-10-25 16:14:00 +0800

    build(backend): add firebase admin sdk to provide firestore db access

--- backend/index.ts
+++ backend/index.ts
@@ -4,17 +4,24 @@
  * Entrypoint
  */
 import express, { Express } from "express";
-import { initialize } from "express-openapi";
+import { initialize as initOpenAPI } from "express-openapi";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
+// setup firebase / firestore db access
+initializeApp({
+  projectId: "flowmotion-4e268",
+});
+const db = getFirestore();
+
+// setup express server
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
-initialize({
+initOpenAPI({
   app,
   apiDoc: "../schema/flowmotion_api.yaml",
   paths: "routes",
 });
-
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
